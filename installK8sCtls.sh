4dd6be32d6f104671f980aca73a36238aa5736e8
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-02 00:30:31 +0530

    Update installK8sCtls.sh

--- installK8sCtls.sh
+++ installK8sCtls.sh
@@ -6,10 +6,12 @@ sudo apt-get install awscli -y
 # kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
-sudo mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+sudo mkdir -p  /usr/local/bin 
+sudo cp ./kubectl  /usr/local/bin/kubectl  
 kubectl version --client
 
 # Helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 sudo chmod 700 get_helm.sh
 ./get_helm.sh
+helm version
